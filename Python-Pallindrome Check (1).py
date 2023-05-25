93514b606b72dedaee74b2d27eb8a9657e39e29d
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-05-25 18:59:38 +0530

    Add files via upload

---
+++ Python-Pallindrome Check (1).py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[16]:
+
+
+word=input("please enter the word/number which you want to check for pallindrome ")
+word=word.lower()
+len_word=len(word)
+reverse=[]
+pallindrome=""
+j=1
+for i in range(len_word):
+    
+#     print(word[i])
+    if word[i]==word[len_word-j]:
+        j=j+1
+        reverse.append(word[i])
+        
+    else:
+        pass
+        
+        
+new_word=pallindrome.join(reverse)
+
+# print(new_word)
+if new_word==word:
+    print(f"Yes the given word/number {word} is pallindrome ")
+else:
+    print(f"No the given word/number {word} is not pallindrome ")
+    
+        
+    
+
+
+# In[ ]:
+
+
+
+
